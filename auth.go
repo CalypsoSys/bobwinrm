f1c3367050e7956f3b400c2a08d117239fee83bf
joe <joseph@calypsosys.com>
2026-08-25 16:44:35 -0400

    fix upstream PR lint errors

--- auth.go
+++ auth.go
@@ -46,7 +46,7 @@ func (c *ClientAuthRequest) Transport(endpoint *Endpoint) error {
 		ResponseHeaderTimeout: endpoint.Timeout,
 	}
 
-	if endpoint.CACert != nil && len(endpoint.CACert) > 0 {
+	if len(endpoint.CACert) > 0 {
 		certPool, err := readCACerts(endpoint.CACert)
 		if err != nil {
 			return err
